2327a7d5a13c972f9fa73e26e3319c375a9be500
AltTheAngel64 <acerenil1322@chih.conalep.edu.mx>
2025-05-28 07:43:33 -0600

    first commit

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "conexion.php";
+$db = "alumnos";
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$pass = $_POST['pass'];
+
+echo "<br>Datos recibidos: <br><br>";
+echo "$nombre, $correo, $pass <br>";
+
+// Enviar consulta SQL
+$sql = "INSERT INTO informacion (id, nombre, correo, pass)
+        VALUES (null, '$nombre', '$correo', '$pass')";
+
+if (mysqli_query($conexion, $sql)) {
+    echo "<br>Nuevo registro creado";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+}
+
+mysqli_close($conexion);
+?>
